480b8760c075a8924530bf2336b945f923aede12
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-01-14 20:17:40 +0000

    Create create_options method.

--- process.py
+++ process.py
@@ -5,7 +5,7 @@ It is likely that most sections will require functions to be placed in this modu
 """
 
 import csv
-from typing import List
+from typing import List, Dict
 
 
 class Review:
@@ -33,3 +33,25 @@ class Process:
                 reviews.append(Review(int(review_id), int(rating), year_month, reviewer_location, branch))
         print('Loading finished!')
         return reviews
+
+    @staticmethod
+    def create_options(items: List[str]) -> Dict[str, str]:
+        """
+            Generates dictionary that maps items to English alphabet letters (A-Z).
+
+            Args:
+                items (List[str]): List of options to be mapped to English alphabet letters.
+
+            Returns:
+                Dict[str, str]: A dictionary of options mapped to letters.
+
+            Raises:
+                ValueError: if items list is longer than 26.
+        """
+        max_len = 26
+        if len(items) > max_len:
+            raise ValueError(f'Options list is too long! Maximum number is: {max_len}')
+        options = {}
+        for i, item in enumerate(items):
+            options[chr(i + 65)] = item
+        return options
