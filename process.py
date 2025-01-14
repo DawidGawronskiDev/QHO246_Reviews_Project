8ba2dace599ee878f7271f2fda998211e287db87
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-01-14 19:56:28 +0000

    Create read_reviews method.

--- process.py
+++ process.py
@@ -4,6 +4,9 @@ perfrom necessary processes in order to provide the desired result in the desire
 It is likely that most sections will require functions to be placed in this module.
 """
 
+import csv
+from typing import List
+
 class Review:
     def __init__(self, review_id: int, rating: int, year_month: str, reviewer_location: str, branch: str):
         self.review_id = review_id
@@ -15,3 +18,16 @@ class Review:
 class Process:
     def __init__(self):
         pass
+
+    @staticmethod
+    def read_reviews(file_path: str) -> List[Review]:
+        print('Loading...')
+        with open(file_path) as f:
+            csvreader = csv.reader(f)
+            csvreader.__next__()
+            reviews = []
+            for review in csvreader:
+                review_id, rating, year_month, reviewer_location, branch = review
+                reviews.append(Review(int(review_id), int(rating), year_month, reviewer_location, branch))
+        print('Loading finished!')
+        return reviews
