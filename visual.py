389d25b333ab4e89cc703d9cb488d1ca2377e752
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-01-21 16:51:54 +0000

    Create Visual class and show_most_reviewed_parks method.

--- visual.py
+++ visual.py
@@ -3,3 +3,28 @@ This module is responsible for visualising the data using Matplotlib.
 Any visualisations should be generated via functions in this module.
 """
 
+from typing import List
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class Visual:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def show_most_reviewed_parks(labels: List[str], vals: List[int], legend: List[str] = None) -> None:
+        fig, ax = plt.subplots()
+
+        # plot values and labels
+        ax.pie(vals, labels=labels)
+
+        # Set title
+        ax.set_title('Most Review Parks')
+
+        # create legend
+        if legend:
+            ax.legend(legend)
+
+        # show figure
+        plt.show()
